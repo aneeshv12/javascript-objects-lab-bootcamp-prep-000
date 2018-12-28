15eecc7b314fd41c911f532fac0e8ed345c26576
Aneesh Venuturumilli <veeaneesh@gmail.com>
2018-12-28 02:26:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
-function 
+function deleteFromObjectByKey(object, key){
+  var dummy = Object.assign({},object);
+  
+}
